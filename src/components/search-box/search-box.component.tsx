a6f533d60c28dfb13f1877761853d531994f9b46
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-01-26 16:47:18 +0300

    type method added

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,5 +1,7 @@
 import './search-box.styles.css';
 
+// İKİ SEÇENEĞİMİZ VAR. interface kullanırsak propları aynı isimler içinde ayrı ayrı yazarsak typescript bunları birleştirir. Veya TYPE kullanırsak bütün propları bir TYPE içinde yazabiliriz.
+
 interface ISearchBoxProps  {
   className: string;
   placeholder?: string; /* ?: means it can be string or null */
@@ -11,9 +13,13 @@ interface ISearchBoxProps {
   ) => void /* if we wont return anything, we can write it void which means in javascript "undefined" */;
 }
 
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: (a:string) => void;
+}
 
-
-const SearchBox = ({ className, placeholder, onChangeHandler }: ISearchBoxProps) => (
+const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
   <input
     className={`search-box ${className}`}
     type='search'
