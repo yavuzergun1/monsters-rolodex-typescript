d0b7882e5081a16ae1fc85975e230aa911932ed2
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-01-26 16:41:59 +0300

    search box converting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
-import SearchBox from './components/search-box/search-box.component';
+import SearchBox from './components/search-box/search-box.component.tsx';
 import './App.css';
 
 const App = () => {
--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,20 +1,24 @@
 import './search-box.styles.css';
 
-interface ISearchBoxProps {
+interface ISearchBoxProps  {
   className: string;
   placeholder?: string; /* ?: means it can be string or null */
 }
 
-interface IChangeHandler {
-onChangeHandler: (a:string) => void /* if we wont return anything, we can write it void which means in javascript "undefined" */
+interface ISearchBoxProps {
+  onChangeHandler: (
+    a: string
+  ) => void /* if we wont return anything, we can write it void which means in javascript "undefined" */;
 }
+
+
 
 const SearchBox = ({ className, placeholder, onChangeHandler }: ISearchBoxProps) => (
   <input
     className={`search-box ${className}`}
     type='search'
     placeholder={placeholder}
-    onChange={onChangeHandler}
+    onChange={(e) => onChangeHandler(e)}
   />
 );
 
